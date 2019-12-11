113e61d877ab43661ede4752fabbb490e91af930
AnnaLi7 <58558464+AnnaLi7@users.noreply.github.com>
2019-12-11 14:39:23 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,8 +1,5 @@
-import time
-
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
     assert browser.find_element_by_class_name("btn-add-to-basket"), "No basket-button on page"
